c03e87232513f067e959ffd077f0eb58fdd80d02
paq1 <p.paquin1605@gmail.com>
2025-01-31 06:28:55 +0100

    refactor(crud_pure_code)

--- src/bin/crud_pure_code.rs
+++ src/bin/crud_pure_code.rs
@@ -1,7 +1,8 @@
 use async_trait::async_trait;
-use futures::lock::{Mutex, MutexGuard};
+use futures::lock::Mutex;
 use std::collections::HashMap;
 use std::sync::Arc;
+use uuid::Uuid;
 
 pub type ResultErr<DATA> = Result<DATA, TrainingError>;
 
@@ -10,7 +11,6 @@ pub enum TrainingError {
     Simple(String),
 }
 
-
 #[derive(Debug, Clone)]
 pub struct Entity<T> {
     data: T,
@@ -18,12 +18,13 @@ pub struct Entity<T> {
     version: u32,
 }
 
-
 #[async_trait]
 pub trait Dao<T> {
-    async fn get_by_id(&self, id: &str) -> ResultErr<Entity<T>>;
+    async fn get_by_id(&self, id: &str) -> ResultErr<Option<Entity<T>>>;
 
     async fn save(&self, item: &T) -> ResultErr<String>;
+
+    async fn upsert(&self, item: &Entity<T>) -> ResultErr<String>;
 }
 
 struct DaoHashMap<T> {
@@ -43,15 +44,15 @@ impl<T> Dao<T> for DaoHashMap<T>
 where
     T: Send + Sync + Clone,
 {
-    async fn get_by_id(&self, id: &str) -> ResultErr<Entity<T>> {
+    async fn get_by_id(&self, id: &str) -> ResultErr<Option<Entity<T>>> {
         let mut guard = self.datas.lock().await;
         let c = guard.get(id);
-        c.map(|e| Ok(e.clone())).unwrap_or(Err(TrainingError::Simple("missing".to_string())))
+        c.map(|e| Ok(Some(e.clone()))).unwrap_or(Ok(None))
     }
 
     async fn save(&self, item: &T) -> ResultErr<String> {
 
-        let id = uuid::Uuid::new_v4().to_string();
+        let id: String = Uuid::new_v4().to_string();
 
         let entity = Entity {
             data: item.clone(),
@@ -59,16 +60,29 @@ where
             version: 1
         };
 
-        let mut guard: MutexGuard<HashMap<String, Entity<T>>> = self.datas.lock().await;
-        guard.insert(id.clone(), entity);
+        self.upsert(&entity).await
+    }
 
-        Ok(id)
+    async fn upsert(&self, item: &Entity<T>) -> ResultErr<String> {
+        let identifiant = item.identifiant.clone();
+        let mut guard = self.datas.lock().await;
+        guard.insert(identifiant.clone(), item.clone());
+        Ok(identifiant)
     }
 }
 
 
-async fn change_something(dao: Arc<dyn Dao<String>>) {
+async fn change_something(dao: Arc<dyn Dao<String>>) -> ResultErr<()> {
+    let inserted = dao.save(&"hello world".to_string()).await?;
+    println!("{:?}", inserted.as_str());
+    let data = dao.get_by_id(inserted.as_str()).await?;
+    if data.is_none() {
+        println!("data is None");
+    } else {
+        println!("data is Some");
+    }
 
+    Ok(())
 }
 
 
@@ -80,5 +94,9 @@ async fn main() -> ResultErr<()> {
     println!("id: {id}");
     let stored_entity = dao.get_by_id(id.as_str()).await?;
     println!("entity : {stored_entity:?}");
+
+    change_something(dao.clone()).await?;
+    change_something(dao).await?;
+
     Ok(())
 }
